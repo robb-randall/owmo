d27fc3eab34c174241653b65c1abc9ae6408b09f
Robb <robb.randall@gmail.com>
2017-06-04 20:09:06 -0500

    Added scripts

--- lib/owmo.rb
+++ lib/owmo.rb
@@ -1,5 +1,25 @@
 require "owmo/version"
+require "owmo/Weather"
 
-module Owmo
-  # Your code goes here...
+
+module OWMO
+  URL = ''
+
+  public
+  def self.weather(params)
+    w = Weather.new params
+    yield w
+  end # weather
+
+  public
+  def self.pretty_json(json)
+    begin
+      json = JSON.parse(json) unless json.is_a? Hash
+      JSON.pretty_generate(json)
+    rescue
+      json
+    end
+  end # pretty_json
+
+
 end
