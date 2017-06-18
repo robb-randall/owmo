5190a7a035673d06bb7b7deb8850aab966ab9f61
Robb <robb.randall@gmail.com>
2017-06-17 23:38:42 -0500

    Added Sinatra example

---
+++ examples/sinatra_example.rb
@@ -0,0 +1,9 @@
+require 'owmo'
+require 'sinatra' # Need to install, not included in gemspec
+require 'uri'
+
+get '/current/:name' do
+    api_key = '<API Key>'
+    weather = OWMO::Weather.new api_key
+    weather.get :current, city_name: params[:name], mode: :html
+end
--- spec/owmo_spec.rb
+++ spec/owmo_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
-RSpec.describe Owmo do
+RSpec.describe OWMO do
   it "has a version number" do
     expect(OWMO::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
