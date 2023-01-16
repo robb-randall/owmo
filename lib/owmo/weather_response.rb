6fdead68c64aee1813f55794c4a58af4871c30c3
Robb Randall <robb.randall@gmail.com>
2023-01-16 09:27:39 -0600

    enhanced examples
    Added guards, fixed a bug with box (doesn't return a `cod` property),
    and fixed rubocop warnings.

--- lib/owmo/weather_response.rb
+++ lib/owmo/weather_response.rb
@@ -10,7 +10,7 @@ module OWMO
     def initialize(http_response)
       # JSON
       @weather = JSON.parse(http_response.body)
-      @code = weather['cod'].to_i
+      @code = parse_code
     rescue JSON::ParserError, TypeError
       # Assume XML or HTML
       @weather = http_response.body
@@ -19,6 +19,18 @@ module OWMO
 
     def error?
       code != 200
+    end
+
+    private
+
+    def parse_code
+      if weather.key? 'cod'
+        weather['cod'].to_i
+      elsif weather.key? 'cnt'
+        weather['cnt'].to_i >= 0 ? 200 : 500
+      else
+        500
+      end
     end
   end
 end
