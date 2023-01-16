8cefbf3dbc3c910b5fa637853ced5e5babcccd72
Robb Randall <robb.randall@gmail.com>
2023-01-16 08:40:09 -0600

    Added tests for coverage

--- spec/owmo/weather_spec.rb
+++ spec/owmo/weather_spec.rb
@@ -20,10 +20,9 @@ RSpec.describe OWMO::Weather do
   describe '#get' do
     let(:api_key) { '12345678901234567890123456789012' }
     let(:weather_response) { Net::HTTPResponse.new(1.0, 200, 'OK') }
-    let(:valid_weather_json) { '{ "cod": "200", "message": "This is an example message text" }' }
-    let(:invalid_weather_json) { '{ "cod": "400", "message": "This is an example message text" }' }
 
     it 'returns weather json' do
+      valid_weather_json = '{ "cod": "200", "message": "This is an example message text" }'
       allow(weather_response).to receive(:body).and_return(valid_weather_json)
 
       owmo = described_class.new(api_key)
@@ -32,7 +31,28 @@ RSpec.describe OWMO::Weather do
       expect(owmo.get(:current, city_name: 'London,UK')).to eq(JSON.parse(valid_weather_json))
     end
 
+    it 'returns weather xml' do
+      valid_weather_xml = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><current></current>'
+      allow(weather_response).to receive(:body).and_return(valid_weather_xml)
+
+      owmo = described_class.new(api_key)
+      allow(owmo).to receive(:http_get).and_return(weather_response)
+
+      expect(owmo.get(:current, city_name: 'London,UK')).to eq(valid_weather_xml)
+    end
+
+    it 'returns weather html' do
+      valid_weather_html = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><current></current>'
+      allow(weather_response).to receive(:body).and_return(valid_weather_html)
+
+      owmo = described_class.new(api_key)
+      allow(owmo).to receive(:http_get).and_return(weather_response)
+
+      expect(owmo.get(:current, city_name: 'London,UK')).to eq(valid_weather_html)
+    end
+
     it 'throws WeatherResponseError exception when provided an invalid API key' do
+      invalid_weather_json = '{ "cod": "400", "message": "This is an example message text" }'
       allow(weather_response).to receive(:body).and_return(invalid_weather_json)
 
       owmo = described_class.new('invalid api_key')
