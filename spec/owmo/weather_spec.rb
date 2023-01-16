d10a7dbd842751872f76cb58e15bad295f357e4a
Robb Randall <robb.randall@gmail.com>
2023-01-16 09:31:24 -0600

    Added tests for coverage

--- spec/owmo/weather_spec.rb
+++ spec/owmo/weather_spec.rb
@@ -21,8 +21,18 @@ RSpec.describe OWMO::Weather do
     let(:api_key) { '12345678901234567890123456789012' }
     let(:weather_response) { Net::HTTPResponse.new(1.0, 200, 'OK') }
 
-    it 'returns weather json' do
+    it 'returns weather json when `cod` is present in the response' do
       valid_weather_json = '{ "cod": "200", "message": "This is an example message text" }'
+      allow(weather_response).to receive(:body).and_return(valid_weather_json)
+
+      owmo = described_class.new(api_key)
+      allow(owmo).to receive(:http_get).and_return(weather_response)
+
+      expect(owmo.get(:current, city_name: 'London,UK')).to eq(JSON.parse(valid_weather_json))
+    end
+
+    it 'returns weather json when `cnt` is present in the response' do
+      valid_weather_json = '{ "cnt": 10, "message": "This is an example message text" }'
       allow(weather_response).to receive(:body).and_return(valid_weather_json)
 
       owmo = described_class.new(api_key)
@@ -53,6 +63,16 @@ RSpec.describe OWMO::Weather do
 
     it 'throws WeatherResponseError exception when provided an invalid API key' do
       invalid_weather_json = '{ "cod": "400", "message": "This is an example message text" }'
+      allow(weather_response).to receive(:body).and_return(invalid_weather_json)
+
+      owmo = described_class.new('invalid api_key')
+      allow(owmo).to receive(:http_get).and_return(weather_response)
+
+      expect { owmo.get(:current, city_name: 'London,UK') }.to raise_error(OWMO::Weather::WeatherResponseError)
+    end
+
+    it 'throws WeatherResponseError exception when response does not include `cod` or `cnt`' do
+      invalid_weather_json = '{ "message": "This is an example message text" }'
       allow(weather_response).to receive(:body).and_return(invalid_weather_json)
 
       owmo = described_class.new('invalid api_key')
